9f7338153b1c7b3ce99b076e266077debe7f1a11
valeeriiiee <obleavalerie@gmail.com>
2024-07-08 00:37:25 +0800

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,48 +1,27 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import css from './ContactForm.module.css';
 import PropTypes from 'prop-types';
 
-export class ContactForm extends Component {
-  static propTypes = {
-    addContact: PropTypes.func.isRequired,
-    contacts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-      })
-    ),
+const ContactForm = ({ addContact, contacts }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = e => {
+    setName(e.target.value);
   };
 
-  state = {
-    name: '',
-    number: '',
+  const handleNumberChange = e => {
+    setNumber(e.target.value);
   };
 
-  handleNameChange = e => {
-    this.setState({
-      name: e.target.value,
-    });
-  };
-
-  handleNumberChange = e => {
-    this.setState({
-      number: e.target.value,
-    });
-  };
-
-  handleSubmit = e => {
-    //   prevent form to refresh after submitting
+  const handleSubmit = e => {
     e.preventDefault();
-    const { name, number } = this.state;
-    const { addContact, contacts } = this.props;
 
-    // cannot submit if name and number is empty
     if (name.trim() === '' || number.trim() === '') {
       return;
     }
-    // alert if existing contact name, prevent submit
+
     const existingContactName = contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
@@ -51,7 +30,6 @@ export class ContactForm extends Component {
       return;
     }
 
-    // alert if existing contact number, prevent submit
     const existingContactNumber = contacts.find(
       contact => contact.number === number
     );
@@ -60,52 +38,58 @@ export class ContactForm extends Component {
       return;
     }
 
-    //   Add contact
     addContact({
       id: nanoid(),
       name: name.trim(),
       number: number.trim(),
     });
 
-    this.setState({
-      name: '',
-      number: '',
-    });
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
+  return (
+    <form className={css.contactForm} onSubmit={handleSubmit}>
+      <label className={css.formInput}>
+        <p className={css.formHeader}>Name</p>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
+          required
+          value={name}
+          onChange={handleNameChange}
+        />
+      </label>
+      <label className={css.formInput}>
+        <p className={css.formHeader}>Number</p>
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handleNumberChange}
+        />
+      </label>
+      <button className={css.contactFormButton} type="submit">
+        Add Contact
+      </button>
+    </form>
+  );
+};
 
-    return (
-      <form className={css.contactForm} onSubmit={this.handleSubmit}>
-        <label className={css.formInput}>
-          <p className={css.formHeader}>Name</p>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
-            required
-            value={name}
-            onChange={this.handleNameChange}
-          />
-        </label>
-        <label className={css.formInput}>
-          <p className={css.formHeader}>Number</p>
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={number}
-            onChange={this.handleNumberChange}
-          />
-        </label>
-        <button className={css.contactFormButton} type="submit">
-          Add Contact
-        </button>
-      </form>
-    );
-  }
-}
+ContactForm.propTypes = {
+  addContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default ContactForm;
